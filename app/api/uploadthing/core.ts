c03103695df71eb892cf2fe3d9b486cb98d7c90e
BENJAMIN IKODIT <bikodit@goldkach.com>
2026-05-08 10:56:56 +0300

    app/api/uploadthing/

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,23 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(async () => {
+      return {};
+    })
+    .onUploadComplete(async ({ file }) => {
+      return { url: file.url };
+    }),
+
+  pdfUploader: f({ pdf: { maxFileSize: "16MB", maxFileCount: 1 } })
+    .middleware(async () => {
+      return {};
+    })
+    .onUploadComplete(async ({ file }) => {
+      return { url: file.url };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
