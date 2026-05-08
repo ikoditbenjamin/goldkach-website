790bb95fdf07568e8f9abda4ae424a8a1b7b17eb
BENJAMIN IKODIT <bikodit@goldkach.com>
2026-05-08 11:58:55 +0300

    public/about/capabilities.png

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -37,7 +37,7 @@ export default function AboutSection() {
           <div className="relative reveal-left">
             <div className="w-full h-full min-h-[480px] overflow-hidden rounded-sm relative">
               <AppImage
-                src="/about/Goldkach-team.png"
+                src="/about/capabilities.png"
                 alt="GoldKach team collaborating"
                 fill
                 className="object-cover"
